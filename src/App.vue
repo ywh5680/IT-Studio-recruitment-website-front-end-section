5036f7d3a722304bc8decc483f0b9391be6fefe0
AsagiYuumoya <Yi_shun@outlook.com>
2025-07-08 13:48:23 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <header class="main-header">
+    <nav class="main-nav">
+      <ul class="nav-list">
+        <li class="nav-item"><a href="#">部门介绍</a></li>
+        <li class="nav-item"><a href="#">项目成果</a></li>
+        <li class="nav-item"><a href="#">团队成员</a></li>
+        <li class="nav-item"><a href="#">关于我们</a></li>
+        <li class="nav-item"><a href="#">留言板</a></li>
+        <li class="nav-item"><a href="#">查询报名进度</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main>
+    <section class="hero">
+      <h1>爱特工作室</h1>
+      <h2>IT-Studio</h2>
+      <div class="actions">
+        <a href="#" class="button button-more">进一步了解</a>
+        <a href="#" class="button button-join">加入招新群</a>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
